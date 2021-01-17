e00298a207f33ffe026ec19ee30df530980afd0f
Stefan <steef2000d@gmail.com>
2021-01-17 02:37:40 +0100

    Cors crap

--- src/EventSubscriber/AuthenticationSubscriber.php
+++ src/EventSubscriber/AuthenticationSubscriber.php
@@ -6,6 +6,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\HttpKernel;
@@ -55,6 +56,29 @@ class AuthenticationSubscriber implements EventSubscriberInterface
         return false;
     }
 
+    private function onRequest(RequestEvent $event) {
+        if (HttpKernel::MASTER_REQUEST != $event->getRequestType()) {
+            return;
+        }
+
+        if($event->getRequest()->isMethod("OPTIONS")) {
+            $event->setResponse(
+                new Response('', 204, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Credentials' => 'true',
+                    'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'DNT, X-User-Token, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type',
+                    'Access-Control-Max-Age' => 1728000,
+                    'Content-Type' => 'text/plain charset=UTF-8',
+                    'Content-Length' => 0
+                ])
+            );
+        }
+
+        $event->stopPropagation();
+
+    }
+
     public function responseController(ResponseEvent $event)
     {
 //        var_dump("Henlo");
@@ -79,6 +103,7 @@ class AuthenticationSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
+            KernelEvents::REQUEST => 'onRequest',
             KernelEvents::CONTROLLER => 'onKernelController',
             KernelEvents::RESPONSE => 'responseController'
         ];
