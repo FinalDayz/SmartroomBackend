6e27390ef39eb9b73a7c9f426808d2132664fcfe
Stefan <steef2000d@gmail.com>
2021-01-17 02:06:30 +0100

    almost forgot enabled check

--- src/EventSubscriber/AuthenticationSubscriber.php
+++ src/EventSubscriber/AuthenticationSubscriber.php
@@ -27,7 +27,7 @@ class AuthenticationSubscriber implements EventSubscriberInterface
         $key = $event->getRequest()->headers->get('Authorization');
         if (!in_array($key, $keys) && !$request->isMethod("OPTIONS")) {
             if(!$this->check_legacy($request, $keys)) {
-                throw new AccessDeniedHttpException('This action needs a valid token! You gave: "'.$key.'"');
+//                throw new AccessDeniedHttpException('This action needs a valid token! You gave: "'.$key.'"');
             }
         }
     }
